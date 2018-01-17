29258d02329c09fad45647f9aa9e60aed4bb5f71
Abnod <ganeroth@gmail.com>
2018-01-17 14:17:48 +0300

    moved database and tables generation code to dao implementation; added readme;

--- src/main/java/abnod/webcart/dao/MySqlDAO.java
+++ src/main/java/abnod/webcart/dao/MySqlDAO.java
@@ -138,4 +138,28 @@ public class MySqlDAO implements DBInterface {
         }
         return orderId;
     }
+
+    public static void initDB(){
+        try (Connection connection = DataSource.getConnection()) {
+            Statement statement = connection.createStatement();
+            statement.execute("CREATE SCHEMA IF NOT EXISTS `webcart` ;");
+            statement.execute(
+                    "CREATE TABLE IF NOT EXISTS `webcart`.`products` (`productId` INT NOT NULL AUTO_INCREMENT, `title` VARCHAR(45) NOT NULL, `description` VARCHAR(200) NULL," +
+                            " `price` FLOAT NOT NULL, PRIMARY KEY (`productId`), UNIQUE INDEX `id_UNIQUE` (`productId` ASC), UNIQUE INDEX `title_UNIQUE` (`title` ASC));"
+            );
+            statement.execute(
+                    "CREATE TABLE IF NOT EXISTS `webcart`.`orders` (`orderId` INT NOT NULL AUTO_INCREMENT, `customerName` VARCHAR(45) NOT NULL, `customerSurname` VARCHAR(45) NOT NULL," +
+                            " `customerPhoneNumber` VARCHAR(45) NOT NULL, PRIMARY KEY (`orderId`), UNIQUE INDEX `id_UNIQUE` (`orderId` ASC));"
+            );
+            statement.execute(
+                    "CREATE TABLE IF NOT EXISTS `webcart`.`ordercontent` (`orderId` INT NOT NULL, `productId` INT NOT NULL, `quantity` INT NOT NULL," +
+                            " INDEX `productId_idx` (`productId` ASC), INDEX `orderId_idx` (`orderId` ASC), PRIMARY KEY (`orderId`, `productId`), CONSTRAINT `productId`" +
+                            " FOREIGN KEY (`productId`) REFERENCES `webcart`.`products` (`productId`) ON DELETE NO ACTION ON UPDATE NO ACTION," +
+                            " CONSTRAINT `orderId` FOREIGN KEY (`orderId`) REFERENCES `webcart`.`orders` (`orderId`) ON DELETE NO ACTION" +
+                            " ON UPDATE NO ACTION);"
+            );
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/abnod/webcart/util/DataSource.java
+++ src/main/java/abnod/webcart/util/DataSource.java
@@ -1,40 +1,18 @@
 package abnod.webcart.util;
 
+import abnod.webcart.dao.MySqlDAO;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 
 import java.sql.Connection;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class DataSource {
     private static HikariConfig config = new HikariConfig("/DataSource.properties");
-    ;
     private static HikariDataSource ds = new HikariDataSource(config);
-    ;
 
     static {
-        try (Connection connection = DataSource.getConnection()) {
-            Statement statement = connection.createStatement();
-            statement.execute("CREATE SCHEMA IF NOT EXISTS `webcart` ;");
-            statement.execute(
-                    "CREATE TABLE IF NOT EXISTS `webcart`.`products` (`productId` INT NOT NULL AUTO_INCREMENT, `title` VARCHAR(45) NOT NULL, `description` VARCHAR(200) NULL," +
-                            " `price` FLOAT NOT NULL, PRIMARY KEY (`productId`), UNIQUE INDEX `id_UNIQUE` (`productId` ASC), UNIQUE INDEX `title_UNIQUE` (`title` ASC));"
-            );
-            statement.execute(
-                    "CREATE TABLE IF NOT EXISTS `webcart`.`orders` (`orderId` INT NOT NULL AUTO_INCREMENT, `customerName` VARCHAR(45) NOT NULL, `customerSurname` VARCHAR(45) NOT NULL," +
-                            " `customerPhoneNumber` VARCHAR(45) NOT NULL, PRIMARY KEY (`orderId`), UNIQUE INDEX `id_UNIQUE` (`orderId` ASC));"
-            );
-            statement.execute(
-                    "CREATE TABLE IF NOT EXISTS `webcart`.`ordercontent` (`orderId` INT NOT NULL, `productId` INT NOT NULL, `quantity` INT NOT NULL," +
-                            " INDEX `productId_idx` (`productId` ASC), INDEX `orderId_idx` (`orderId` ASC), PRIMARY KEY (`orderId`, `productId`), CONSTRAINT `productId`" +
-                            " FOREIGN KEY (`productId`) REFERENCES `webcart`.`products` (`productId`) ON DELETE NO ACTION ON UPDATE NO ACTION," +
-                            " CONSTRAINT `orderId` FOREIGN KEY (`orderId`) REFERENCES `webcart`.`orders` (`orderId`) ON DELETE NO ACTION" +
-                            " ON UPDATE NO ACTION);"
-            );
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        init();
     }
 
     private DataSource() {
@@ -42,5 +20,12 @@ public class DataSource {
 
     public static Connection getConnection() throws SQLException {
         return ds.getConnection();
+    }
+
+    /**
+     * override this for change database creation dao
+     */
+    public static void init(){
+        MySqlDAO.initDB();
     }
 }
